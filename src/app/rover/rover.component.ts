40c9152c937b204a915dfc45275b394ca257829c
bearaxe <insetpun@gmail.com>
2017-08-11 21:30:11 -0400

    rover moves dynamicaly

--- src/app/rover/rover.component.ts
+++ src/app/rover/rover.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-rover',
   templateUrl: './rover.component.html',
   styleUrls: ['./rover.component.css']
 })
-export class RoverComponent implements OnInit {
+export class RoverComponent implements OnInit, OnChanges {
+  @Input() size;
+  @Input() x;
+  @Input() y;
 
   constructor() { }
+
+  ngOnChanges() {
+    console.log('ayy found somethin')
+  }
 
   ngOnInit() {
   }
