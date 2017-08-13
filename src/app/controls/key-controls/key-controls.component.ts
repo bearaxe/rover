c0969a00c71fbecc0dc4c8787f4c88cd5cb0344c
bearaxe <insetpun@gmail.com>
2017-08-13 14:24:18 -0400

    add dirtmask layer and improve arrow key controls

--- src/app/controls/key-controls/key-controls.component.ts
+++ src/app/controls/key-controls/key-controls.component.ts
@@ -10,7 +10,16 @@ import { KeyHandlerService } from '../../key-handler.service';
 export class KeyControlsComponent implements OnInit {
   keytype = 'akey';
 
-  constructor(private key: KeyHandlerService) { }
+  constructor(private key: KeyHandlerService) {
+    // This disables all arrowkey functions
+    // This fixes the browser jump bug and the control select hijack bug
+    window.addEventListener("keydown", function(e) {
+        // space and arrow keys
+        if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+            e.preventDefault();
+        }
+    }, false);
+  }
 
   ngOnInit() {
     this.key.setKeyType(this.keytype);
