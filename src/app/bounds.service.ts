8a7f5289d4e353aa17c8006bf165dab22994331d
bearaxe <insetpun@gmail.com>
2017-08-14 22:09:11 -0400

    less logs

--- src/app/bounds.service.ts
+++ src/app/bounds.service.ts
@@ -14,20 +14,18 @@ export class BoundsService {
   xArr = [];
   yArr = [];
 
-  constructor() {
-    // this.createObsticals();
-  }
+  constructor() {}
 
   checkMove(pos, destination, key){
-    console.log('xarr:', this.xArr);
-    console.log('yarr:', this.yArr);
+    // console.log('xarr:', this.xArr);
+    // console.log('yarr:', this.yArr);
     // console.log('pos:', pos, ' dest:', destination, ' key:', key);
     // console.log('comparing to base:', this.basis, ' width:', this.width, ' height:', this.height);
     if(key === 'x'){
       // the other way to write this is way harder to read, so leaving for now.
       if(destination <= this.width && destination >= 0){ // if within bounds
         if(!this.xArr.includes(destination)){ //if you're not going to be in the x array
-          console.log('dest x not in xArr');
+          // console.log('dest x not in xArr');
           return destination
         } else if(pos.y !== this.yArr[this.xArr.indexOf(destination)]){ //or, if you are, if you're not matching a forbidden coordinate
           const possibleHits = this.getAllIndexes(this.xArr, destination);
@@ -36,8 +34,8 @@ export class BoundsService {
               return pos.x
             }
           }
-          console.log('dest x in xArr, but pos.y !== this.yArr', pos.y !== this.yArr[this.xArr.indexOf(destination)]);
-          console.log('db read: posy = ', pos.y, ':: this.yArr[inddest] = ', this.yArr[this.xArr.indexOf(destination)]);
+          // console.log('dest x in xArr, but pos.y !== this.yArr', pos.y !== this.yArr[this.xArr.indexOf(destination)]);
+          // console.log('db read: posy = ', pos.y, ':: this.yArr[inddest] = ', this.yArr[this.xArr.indexOf(destination)]);
           return destination
         }
       }
@@ -47,7 +45,7 @@ export class BoundsService {
       // return (destination <= this.height && destination >= 0? destination : pos.y);
       if(destination <= this.height && destination >= 0){ // if within bounds
         if(!this.yArr.includes(destination)){ //if you're not going to be in the x array
-          console.log('dest y not in yArr');
+          // console.log('dest y not in yArr');
           return destination
         } else {//if(pos.x !== this.xArr[]){ //or, if you are, if you're not matching a forbidden coordinate
           const possibleHits = this.getAllIndexes(this.yArr, destination);
@@ -56,23 +54,21 @@ export class BoundsService {
               return pos.y
             }
           }
-          console.log('dest y in yArr, but pos.x !== this.xArr', pos.x !== this.xArr[this.yArr.indexOf(destination)]);
-          console.log('db read: posx = ', pos.x, ':: this.xArr[inddest] = ', this.xArr[this.yArr.indexOf(destination)]);
+          // console.log('dest y in yArr, but pos.x !== this.xArr', pos.x !== this.xArr[this.yArr.indexOf(destination)]);
+          // console.log('db read: posx = ', pos.x, ':: this.xArr[inddest] = ', this.xArr[this.yArr.indexOf(destination)]);
           return destination
         }
       }
       return pos.y;
     }
-    // return destination;
   }
 
   createObsticals(){
-    // if(this.width === undefined){ setTimeout(()=>{this.createObsticals()},1)}
     let i = 0;
     while(i <= this.obsticalAmount){
       // I'm storing it like this so I can be lazy while checking
       // it may not be the most efficient way, but it /will/ work
-      // I was wrong, my lazy way lead to it stopping at first lol
+      // I wasnt 100% right, my lazy way lead to it not checking all options
       let randX = this.rand(0, this.baseWidth);
       let randY = this.rand(0, this.baseHeight);
       while(randX === 0 && randY === 0){ //if rand is start position, reroll
